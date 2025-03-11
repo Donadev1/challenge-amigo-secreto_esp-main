3b27342fb68707d40cdf1f46f561ceaf1e33ffd8
Tristan Donado <tristanuni2701@gmail.com>
2025-03-10 22:36:41 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,48 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    const IngresarNombre = document.getElementById("amigo");
+    const listaAmigos = document.getElementById("listaAmigos");
+
+    if (IngresarNombre.value.trim() === "") { 
+        alert("Por favor ingrese un nombre");
+        return;
+    }
+    amigos.push(IngresarNombre.value.trim());
+
+    // Limpiar el campo de entrada
+    IngresarNombre.value = "";
+    listaAmigos.innerHTML = "";
+
+    listaAmigos.style.display = "block";    
+
+    // Agregar cada amigo a la lista
+    for (let i = 0; i < amigos.length; i++) {
+        let li = document.createElement("li");
+        li.textContent = amigos[i];
+        listaAmigos.appendChild(li);
+    }
+
+    console.log(amigos); 
+}
+
+function sortearAmigo() {
+   
+        if (amigos.length === 0) {
+            alert("No hay amigos para buscar");
+            return;
+        }
+
+        const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+
+        const amigoSorteado = amigos.splice(indiceAleatorio, 1)[0];
+        document.getElementById("resultado").textContent = "Amigo sorteado: " + amigoSorteado;
+    
+        document.getElementById("listaAmigos").style.display = "none";
+        console.log(amigos); 
+    }
+    
+
+
+
